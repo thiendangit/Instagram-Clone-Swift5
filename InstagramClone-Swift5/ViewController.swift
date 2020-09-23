b81bb81a840c42c996e1136217b2b6ccddbb219f
macintoshhd <1234>
2020-09-23 11:06:53 +0700

    first commit

---
+++ InstagramClone-Swift5/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InstagramClone-Swift5
+//
+//  Created by Thiện Đăng on 9/23/20.
+//  Copyright © 2020 Thiện Đăng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
